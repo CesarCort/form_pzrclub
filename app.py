95d8d7a4b9e1c543259306a05833805442cb9f16
Cesar Cort <cesarunner3@gmail.com>
2021-07-09 23:08:29 -0500

    New Template Integration

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ app.config['MYSQL_PORT'] = 3306
 app.config['MYSQL_USER'] = 'admin'
 app.config['MYSQL_PASSWORD'] = '11235813::'
 app.config['MYSQL_DB'] = 'users'
- 
+
 # mysql = MySQL(app)
 
 
@@ -63,7 +63,7 @@ def home():
     headers = data.pop(0)
     df_validation = pd.DataFrame(data, columns=headers)
     #nro_delivery_agent = list_option(df_validation,column="nro_delivery_agent")
-    
+
     # product_list
     product_list = list_option(df_validation,column="product")
     quantity_product_list = list_option(df_validation,column="quantity_product")
@@ -76,9 +76,10 @@ def home():
     checkout_status_list = list_option(df_validation,column="checkout_status")
 
     #nro_delivery_agent = list_option(df_validation,column="nro_delivery_agent")
-    
-    
-    return render_template('form_pzr.html',product_list=product_list,country_type_list=country_type_list,
+
+
+    return render_template('new-order-created.html',#'form_pzr.html',
+                            product_list=product_list,country_type_list=country_type_list,
                           quantity_product_list=quantity_product_list,
                           purchase_channel_list=purchase_channel_list,
                           delivery_status_list=delivery_status_list,
@@ -89,25 +90,28 @@ def home():
 @app.route("/process",methods=["GET","POST"])
 def form():
     print(request.method)
+    data = request.form
+    data = data.to_dict()
     print(request.form)
     if request.method == "GET":
         return "Invalid action"
     if str(request.method) == "POST":
+        customer_name    = data["costumer_name"]
         
-        customer_name    = request.form["costumer_name"]
-        customer_phone   = request.form["costumer_phone"]
-        
-        product_name   = request.form["product_name"]
-        purchase_channel = request.form["purchase_channel"]
-        delivery_address = request.form["delivery_address"]
-        link_delivery_address = request.form["link_delivery_address"]
-        country_type = request.form["country_type"]
-        
-        delivery_status = request.form["delivery_status"]
-        delivery_type = request.form["delivery_type"] # new
-        delivery_agent = request.form["delivery_agent"] # new
-        
-        checkout_status = request.form["checkout_status"]
+        customer_phone   = data["costumer_phone"]
+
+        product_name   = data["product_name"]
+        quantity_product  = data["quantity_product"]
+        purchase_channel = data["purchase_channel"]
+        delivery_address = data["delivery_address"]
+        link_delivery_address = data["link_delivery_address"]
+        country_type = data["country_type"]
+
+        delivery_status = data["delivery_status"]
+        delivery_type = data["delivery_type"] # new
+        delivery_agent = data["delivery_agent"] # new
+
+        checkout_status = data["checkout_status"]
         print(checkout_status)
         print("hola")
         # MYSQL INSERT
@@ -126,25 +130,25 @@ def form():
         #     msg = 'Please fill out the form!'
         # else:
             # MYSQL INSERT
-            
-            
+
+
             # cursor.execute("""INSERT INTO account(id,username,name,phone,email,password) VALUES(NONE,%s,%s,%s,%s,MD5(%s))""",
             #            (username ,name ,phone ,email ,password))
-            
+
             # mysql.connection.commit()
             # msg = "Registro exitoso"
-            
+
             # Set the webhook_url to the one provided by Slack when you create the webhook at https://my.slack.com/services/new/incoming-webhook/
         webhook_url = 'https://hook.integromat.com/t4rg4pccv3mg53w7qr4xudlrmbtlu31o'
         #slack_data = {'text': "Sup! We're hacking shit together @HackSussex :spaghetti:"}
-        
-        json_data_form = json.dumps(request.form)
+
+        json_data_form = json.dumps(data)
         print(json_data_form)
         response = requests.post(
             webhook_url, data=json_data_form,
             headers={'Content-Type': 'application/json'}
                 )
-        
+
         # text =   """%2ANombre de cliente%2A : {}%0D%0A
         #             %2ANúmerode contacto%2A : {}%0D%0A
         #             %2AProducto%2A : {}%0D%0A
@@ -155,13 +159,13 @@ def form():
         #                         product_name,purchase_channel,
         #                         delivery_address,link_delivery_address)
         msg_costumer = "Pizarra Club - Detalle Pedido"
-        text_agent =   """%2ANombre de cliente%2A : {}%0D%0A%2ANúmerode contacto%2A : {}%0D%0A%2AProducto%2A : {}%0D%0A%2ACanal de compra%2A : {}%0D%0A%2ADirección de envío%2A : {}%0D%0A%2ALink de dirección%2A : {}%0D%0A%2ATIPO DE ENVIO%2A : {}%0D%0A%2AAgente asignado%2A : {}%0D%0A""".format(customer_name,customer_phone,
-                                product_name,purchase_channel,
+        text_agent =   """%2ANombre de cliente%2A : {}%0D%0A%2ANúmerode contacto%2A : {}%0D%0A%2AProducto%2A : {}%0D%0A%2ACantidad%2A : {}%0D%0A%2ACanal de compra%2A : {}%0D%0A%2ADirección de envío%2A : {}%0D%0A%2ALink de dirección%2A : {}%0D%0A%2ATIPO DE ENVIO%2A : {}%0D%0A%2AAgente asignado%2A : {}%0D%0A""".format(customer_name,customer_phone,
+                                product_name,quantity_product,purchase_channel,
                                 delivery_address,link_delivery_address,delivery_type,delivery_agent)
-        text_customer =   """%2A===================%2A%0D%0A{}%0D%0A%2AProducto%2A : {}%0D%0A%2ACanal de compra%2A : {}%0D%0A%2ADirección de envío%2A : {}%0D%0A%2ALink de dirección%2A : {}%0D%0A%2ATIPO DE ENVIO%2A : {}%0D%0A""".format(msg_costumer,product_name,purchase_channel,
+        text_customer =   """%2A===================%2A%0D%0A{}%0D%0A%2ACliente%2A : {}%0D%0A%2AProducto%2A : {}%0D%0A%2ACanal de compra%2A : {}%0D%0A%2ADirección de envío%2A : {}%0D%0A%2ALink de dirección%2A : {}%0D%0A%2ATIPO DE ENVIO%2A : {}%0D%0A""".format(msg_costumer,customer_name,product_name,purchase_channel,
                                 delivery_address,link_delivery_address,delivery_type)
-        
-        
+
+
         text_agent = text_agent.replace(" ","%20")
         text_customer = text_customer.replace(" ","%20")
         print(text_customer)
@@ -183,11 +187,15 @@ def form():
                 'Request to slack returned an error %s, the response is:\n%s'
                 % (response.status_code, response.text)
             )
-        
-        return render_template("success_page.html",wsp_link_agent=wsp_link_agent,wsp_link_customer=wsp_link_customer,success_msg=success_msg)
-        
-        
-        
+
+        return render_template("thanks-order.html",#"success_page.html",
+                               wsp_link_agent=wsp_link_agent,
+                               wsp_link_customer=wsp_link_customer,
+                               success_msg=success_msg,
+                               delivery_type=delivery_type)
+
+
+
 
 
 
@@ -198,5 +206,3 @@ def user_profile(user_id):
 if __name__=="__main__":
     port = int(os.environ.get("PORT",5000))
     app.run(host="0.0.0.0",port=port)
-
-
